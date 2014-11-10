399d52f4819bb6ecc69a68ff8e79a8dd40dc7590
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2014-11-10 11:53:04 +0800

    More helpful CLI.

--- lib/cli.ex
+++ lib/cli.ex
@@ -3,18 +3,37 @@ require Logger
 defmodule Blitzy.CLI do
 
   def main(args) do
-    options = args |> parse_args
-
-    do_requests(options[:url], options[:requests])
+    args |> parse_args |> process_options
   end
 
   defp parse_args(args) do
-    {[requests: n], [url], []} = OptionParser.parse(args, aliases: [n: :requests])
-    %{url: url, requests: String.to_integer(n)}
+    OptionParser.parse(args, aliases: [n: :requests])
+  end
+
+  defp process_options(options) do
+    case options do
+      {[requests: n], [url], []} ->
+        do_requests(url, n)
+      _ -> do_help
+    end
   end
 
   defp do_requests(url, requests) do
     Logger.info "Pummelling #{url} with #{requests} requests"
+  end
+
+  defp do_help do
+    IO.puts """
+      Usage:
+        blitzy -n [requests] [url]
+ 
+      Options:
+        -n, [--requests]      # Number of requests
+ 
+      Example:
+        ./blitzy -n 100 http://www.example.com
+    """
+    System.halt(0)
   end
 
 end
